69bd61a5f93009a6e0af55a99c403441be7902a3
radrybinski <rybinski.radoslaw@gmail.com>
2025-01-21 14:36:11 +0100

    training day

--- Program.cs
+++ Program.cs
@@ -1,51 +1,285 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+using System;
+using System.Drawing;
 
-
-Console.WriteLine("My C# Training App");
-
-Car car1 = new Car();
-Car car2 = new Car();
-
-Console.WriteLine(car1.color);
-car1.color = "blue";
-Console.WriteLine(car1.color);
-car1.maxSpeed = 123;
-
-car1.DriveFullSpeed();
-
-Console.WriteLine(car1.model);
-Console.WriteLine(car2.model);
-
-
-
-class Car
+namespace Training
 {
-    //default it is set to  private
-    public string color = "red";
-    public string model;
-    public int maxSpeed;
-    public int year;
 
-    public Car() 
+    class Program
     {
-        model = "Mustang";
-    
-    
+
+        enum WeekDays
+        {
+            Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+
+        }
+
+
+        //test Struct
+        struct myPoint
+        {
+            private double x;
+            private double y;
+            private double z;
+
+            private string structName;
+
+
+            public myPoint(double xDim, double yDim, double zDim, string name)
+            {
+
+                x = xDim;
+                y = yDim;
+                z = zDim;
+                structName = name;
+
+
+
+            }
+
+
+            public void Print()
+            {
+
+                Console.WriteLine("Point details: X = {0}, Y = {1}, Z = {2}, name = {3}", x, y, z, structName);
+
+            }
+
+
+        }
+
+
+        class Worker
+        {
+            private int hourlyRate = 30;
+            private string name;
+            private int hoursWorked;
+
+            public int HoursWorked
+            {
+                get
+                {
+
+                    return hoursWorked;
+                }
+
+                set
+                {
+
+                    if (value >= 160)
+                    {
+
+                        hoursWorked = 160;
+
+                    }
+                    else
+                    {
+                        hoursWorked = value;
+
+
+                    }
+
+
+
+                }
+            }
+
+            public void Payroll()
+            {
+
+                int hRate = hourlyRate;
+                int hWorked = hoursWorked;
+                int payroll = hWorked * hRate;
+
+                Console.WriteLine($"Payroll = {payroll} ");
+
+
+            }
+
+        }
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            string path = "d:\\test.txt";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (sr.EndOfStream != true)
+                {
+
+                    Console.WriteLine(sr.ReadLine());
+
+
+                }
+
+
+                sr.Close();
+
+
+            }
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine("Add text1");
+                sw.WriteLine("Add text2");
+                sw.WriteLine("Add text3");
+                sw.WriteLine("The end");
+
+            }
+
+
+
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (sr.EndOfStream != true)
+                {
+
+                    Console.WriteLine(sr.ReadLine());
+
+                }
+
+                sr.Close();
+
+            }
+
+
+
+
+
+
+            /*
+            //LINQ test
+            int[] myArray = { 0, 1, 2, 3, 4, 5, 6, 7 };
+
+            var testQuery =
+                from num in myArray
+                where (num % 2) == 0
+                select num;
+
+
+            foreach (int i in testQuery)
+            {
+                Console.WriteLine("{0} is even number ", i);
+
+
+            }
+
+
+            */
+
+
+
+
+
+            /*
+
+            // worker class
+            Worker worker1 = new Worker();
+            Console.WriteLine("Input number of hours:");
+            int numberOfHoures = Convert.ToInt32(Console.ReadLine());
+            worker1.HoursWorked = numberOfHoures;
+            worker1.Payroll();
+
+            */
+
+
+            /* struct
+            WeekDays today = WeekDays.Friday;
+
+            Console.WriteLine((WeekDays)4);
+
+            WeekDays startDay = WeekDays.Monday;
+
+            myPoint point1 = new myPoint (10,20, 30,"Point-1");
+
+           point1.Print();
+
+            */
+
+
+            /* class
+            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("My C# Training App");
+
+            Car car1 = new Car();
+            Car car2 = new Car();
+
+            Console.WriteLine(car1.color);
+            car1.color = "blue";
+            Console.WriteLine(car1.color);
+            car1.maxSpeed = 123;
+
+            car1.DriveFullSpeed();
+
+            Console.WriteLine(car1.model);
+            Console.WriteLine(car2.model);
+            */
+
+        }
+
+
+
+
     }
 
 
-
-    public void DriveFullSpeed()
+    class Car
     {
-        Console.WriteLine($"Car speed is {maxSpeed}");
+        //default it is set to  private
+        public string color = "red";
+        public string model;
+        public int maxSpeed;
+        public int year;
+
+        public Car()
+        {
+            model = "Mustang";
+
+
+        }
+
+
+
+        public void DriveFullSpeed()
+        {
+            Console.WriteLine($"Car speed is {maxSpeed}");
+
+
+        }
+
+
 
 
     }
-        
-       
-
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
