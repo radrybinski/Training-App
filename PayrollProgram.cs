4134e3ce04bd5692b6ad703967ca3ddc7a3fc3cc
radrybinski <rybinski.radoslaw@gmail.com>
2025-02-03 16:25:40 +0100

    finished code

--- PayrollProgram.cs
+++ PayrollProgram.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
 using System.Transactions;
 
 namespace PayrollSoftware
@@ -13,7 +15,7 @@ namespace PayrollSoftware
         //Properties
         public float TotalPay { get; protected set; }
         public float BasicPay { get; private set; }
-        public string NameOfStaff { get; set; }
+        public string NameOfStaff { get; private set; }
         public int HouresWorked
         {
             get { return _hWorked; }
@@ -55,7 +57,7 @@ namespace PayrollSoftware
 
         public override string ToString()
         {
-            return $"Staff name: {NameOfStaff}, hRate: {_hourlyRate}, hWorked: {_hWorked}, total pay: {TotalPay} .";
+            return $"Staff name: {NameOfStaff}, \nhRate: {_hourlyRate}, \nhWorked: {_hWorked}, \ntotal pay: {TotalPay} .";
 
 
         }
@@ -64,42 +66,20 @@ namespace PayrollSoftware
 
     class Manager : Staff
     {
-        private const float managerHourlyRate = 50;
+        private const float managerHourlyRate = 50f;
 
 
-        public int Allowance
-        {
-            get;
-            private set;
-        }
+        public int Allowance {get; private set;}
 
 
-        public float TotalPay
-        {
-            get { return base.TotalPay; }
-            set
-            {
-                if (HouresWorked > 160)
-                {
-                    TotalPay = BasicPay + Allowance;
-
-                }
-                else
-                {
-                    TotalPay = BasicPay;
-
-                }
-            }
-        }
+  
 
 
         public Manager(string name) : base(name, managerHourlyRate)
         {
-
             //NameOfStaff = name;
             //base.NameOfStaff = name;
             //= managerHourlyRate;
-
         }
 
 
@@ -109,12 +89,17 @@ namespace PayrollSoftware
             base.CalculatePay();
             Allowance = 1000;
 
+            if (HouresWorked > 160)
+            {
+                TotalPay = BasicPay + Allowance;
+
+            }
 
         }
 
         public override string ToString()
         {
-            return $"Manager name: {NameOfStaff}, hRate: {managerHourlyRate}, hWorked: {HouresWorked}, total pay: {TotalPay} .";
+            return $"Manager name: {NameOfStaff}, \nhRate: {managerHourlyRate}, \nhWorked: {HouresWorked}, \ntotal pay: {TotalPay} .";
         }
 
 
@@ -122,8 +107,8 @@ namespace PayrollSoftware
 
     class Admin : Staff
     {
-        private const float overtimeRate = 15.5F;
-        private const float adminHourlyRate = 30;
+        private const float overtimeRate = 15.5f;
+        private const float adminHourlyRate = 30f;
         public Admin(string name) : base(name, adminHourlyRate)
         {
 
@@ -149,7 +134,7 @@ namespace PayrollSoftware
 
         public override string ToString()
         {
-            return $"Admin name: {NameOfStaff}, hRate: {adminHourlyRate}, hWorked: {HouresWorked}, total pay: {TotalPay} .";
+            return $"Admin name: {NameOfStaff}, \nhRate: {adminHourlyRate}, \nhWorked: {HouresWorked}, \ntotal pay: {TotalPay} .";
         }
 
     }
@@ -231,50 +216,84 @@ namespace PayrollSoftware
 
 
 
-        public PaySlip( int payMonth, int payYear )
+        public PaySlip(int payMonth, int payYear)
         {
-        month = payMonth;   
-        year = payYear; 
-        
-        
+            month = payMonth;
+            year = payYear;
+
+
         }
 
 
 
-        public void GeneratePaySlip(List<Staff> myStuff)
+        public void GeneratePaySlip(List<Staff> myStaff)
         {
             string path;
 
-            foreach(Staff f in myStuff)
+
+
+            foreach (Staff f in myStaff)
             {
-                
-                path = Path.Join(f.NameOfStaff ,".txt");
+                path = Path.Join(f.NameOfStaff, ".txt");
 
-                using (StreamWriter sw = new StreamWriter(path,false))
-
-
-
-
-
+                if (f is Manager)
                 {
-             
 
-                    sw.WriteLine("PAYSLIP FOR {0} {1}", (MonthsOfYear)month, year);
-                    sw.WriteLine("===============================");
-                    sw.WriteLine($"Name of Staff: {0}", f.NameOfStaff);
-                    sw.WriteLine($"Hours Worked: {0}", f.HouresWorked);
-                    sw.WriteLine();
-                    sw.WriteLine($"Basic Pay: {0}", f.BasicPay);
-                    sw.WriteLine($"Allowance: $1000");
-                    sw.WriteLine();
-                    sw.WriteLine("===============================");
-                    sw.WriteLine($"Total Pay: {0}", f.TotalPay);
-                    sw.WriteLine("===============================");
+                    using (StreamWriter sw = new StreamWriter(path, false))
+
+                    {
+
+                        sw.WriteLine("PAYSLIP FOR {0} {1}", (MonthsOfYear)month, year);
+                        sw.WriteLine("===============================");
+                        sw.WriteLine($"Name of Staff: {0}", f.NameOfStaff);
+                        sw.WriteLine($"Hours Worked: {0}", f.HouresWorked);
+                        sw.WriteLine();
+                        sw.WriteLine($"Basic Pay: {0}", f.BasicPay);
+                        sw.WriteLine($"Allowance: {0}", ((Manager)f).Allowance);
+                        sw.WriteLine();
+                        sw.WriteLine("===============================");
+                        sw.WriteLine($"Total Pay: {0}", f.TotalPay);
+                        sw.WriteLine("===============================");
 
 
-                    Console.WriteLine("----");
+                        sw.Close();
+
+                    }
 
                 }
+                else if (f is Admin)
+                {
+                    using (StreamWriter sw = new StreamWriter(path, false))
+
+                    {
+
+                        sw.WriteLine("PAYSLIP FOR {0} {1}", (MonthsOfYear)month, year);
+                        sw.WriteLine("===============================");
+                        sw.WriteLine($"Name of Staff: {0}", f.NameOfStaff);
+                        sw.WriteLine($"Hours Worked: {0}", f.HouresWorked);
+                        sw.WriteLine();
+                        sw.WriteLine($"Basic Pay: {0}", f.BasicPay);
+                        sw.WriteLine($"Overtime Pay: {0}", ((Admin)f).Overtime);
+                        sw.WriteLine();
+                        sw.WriteLine("===============================");
+                        sw.WriteLine($"Total Pay: {0}", f.TotalPay);
+                        sw.WriteLine("===============================");
+
+
+
+
+
+                        sw.Close();
+                    }
+
+
+
+
+                }
+
+
+
+
 
 
             }
@@ -282,27 +301,159 @@ namespace PayrollSoftware
 
         }
 
+        public void GenerateSummary(List<Staff> myStaff)
 
-
-    }
-
-
-
-
-    class Program
-    {
-
-        static void Main(string[] args)
         {
+            var employee10houres =
+                from employee in myStaff
+                where employee.HouresWorked <= 10
+                orderby employee.NameOfStaff ascending
+                select new { employee.NameOfStaff, employee.HouresWorked };
 
-            Console.WriteLine("Start..!");
 
-            Console.WriteLine("----");
+            string path = "summary.txt";
+
+            using (StreamWriter sw = new StreamWriter(path, false))
+
+            {
+                sw.WriteLine("Staff with less than 10 working hours");
+
+                foreach (var s in employee10houres)
+                {
+                    sw.WriteLine($"Name of Staff: {0}, Hours Worked: {1}", s.NameOfStaff, s.HouresWorked);
+                }
+
+
+
+                sw.Close();
+
+            }
+
+
+        }
+
+        public override string ToString()
+        {
+            //not ready yet
+            return base.ToString();
         }
 
 
+        class Program
+        {
+
+
+
+
+            static void Main(string[] args)
+            {
+
+                List<Staff> myStaff = new List<Staff>();
+
+                FileReader fr = new FileReader();
+                int month = 0;
+                int year = 0;
+
+
+                while (year == 0)
+                {
+                    Console.Write("\nPlease enter the year:");
+
+                    try
+                    {
+                        string input = Console.ReadLine();
+                        year = Convert.ToInt32(input);
+
+
+
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message + "Wrong input, try again!");
+
+                    }
+
+                }
+
+                while (month == 0)
+                {
+                    Console.Write("\nPlease enter the month number:");
+
+                    try
+                    {
+                        string input = Console.ReadLine();
+
+                        int inputConverted = Convert.ToInt32(input);
+
+                        if (inputConverted > 0 || inputConverted <= 12)
+                        {
+                            month = inputConverted;
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Month number is incorrect");
+                        }
+
+
+
+
+
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Wrong month format");
+
+                    }
+
+                }
+
+
+                myStaff = fr.ReadFile();
+
+                for (int i = 0; i < myStaff.Count; i++)
+                {
+                    try
+                    {
+                        Console.WriteLine($"Enter hours worked for {0}", myStaff[i].NameOfStaff);
+                        string input = Console.ReadLine();
+                        int inputConverted = Convert.ToInt32(input);
+                        myStaff[i].HouresWorked = inputConverted;
+                        myStaff[i].CalculatePay();
+                        Console.WriteLine(myStaff[i].ToString());
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+
+                        i--;
+                    }
+
+                }
+
+                PaySlip ps = new PaySlip(month, year);
+                ps.GeneratePaySlip(myStaff);
+                ps.GenerateSummary(myStaff);
+
+                Console.Read(); //wait till exit
+
+
+
+            }
+
+
+
+
+        }
+
+
+
     }
 
 
-
 }
+
+
+
